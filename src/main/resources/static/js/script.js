9fcc431c26174f5dcc2d70be9f83eb08130a44ab
Nicolas V Silva <nvs.silva10@gmail.com>
2025-11-23 15:53:43 -0300

    arrumando o filtro de busca

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -91,9 +91,8 @@ function renderizarProdutosBuscados() {
 
         if (
             p.nome.toLowerCase().includes(termo) ||
-            (p.descricao && p.descricao.toLowerCase().includes(termo)) ||
-            p.marca.toLowerCase().includes(termo) ||
-            p.preco.toString().includes(termo)
+            p.descricao.toLowerCase().includes(termo) ||
+            p.marca.toLowerCase().includes(termo)
         )
 
         {
